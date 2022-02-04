f5a5c6974b4e0afed634cb32fa810c400f823fb8
Josue Alejo <jalejo@cikume.com>
2022-02-03 20:33:51 -0600

    Creating new components, stylesheets, custom pipes, using directives and data binding

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
