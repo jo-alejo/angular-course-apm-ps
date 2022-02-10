f7e1b032bb1e78b229e4d01461f832813f3843d1
Josue Alejo <jalejo@cikume.com>
2022-02-10 13:59:59 -0600

    Adding a service to handle data and setting it available to all components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './product.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { IProduct } from './product';
 
@@ -14,9 +15,11 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
 
     private _listFilter: string = '';
+
     get listFilter(): string {
         return this._listFilter;
     }
+
     set listFilter(value: string) {
         this._listFilter = value;
         console.log('In setter: ', value);
@@ -24,49 +27,9 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
 
-    products: IProduct[] = [
-        {
-            'productId': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2021',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageUrl': 'assets/images/leaf_rake.png'
-        },
-        {
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2021',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageUrl': 'assets/images/garden_cart.png'
-        },
-        {
-            'productId': 5,
-            'productName': 'Hammer',
-            'productCode': 'TBX-0048',
-            'releaseDate': 'May 21, 2021',
-            'description': 'Curved claw steel hammer',
-            'price': 8.9,
-            'starRating': 4.8,
-            'imageUrl': 'assets/images/hammer.png'
-        },
-        {
-            'productId': 8,
-            'productName': 'Saw',
-            'productCode': 'TBX-0022',
-            'releaseDate': 'May 15, 2021',
-            'description': '15-inch steel blade hand saw',
-            'price': 11.55,
-            'starRating': 3.7,
-            'imageUrl': 'assets/images/saw.png'
-        },
-    ];
+    constructor(private productService: ProductService) { }
 
 
     toggleImage(): void {
@@ -74,7 +37,8 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart'
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string): IProduct[] {
